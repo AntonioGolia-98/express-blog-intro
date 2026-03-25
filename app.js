d1725f4c3300397287bef88370b7fd099eff6009
AntonioGolia-98 <antoniog98@icloud.com>
2026-03-25 18:55:50 +0100

    rotta bacheca con array

--- app.js
+++ app.js
@@ -6,6 +6,48 @@ app.use(express.static("public"));
 
 //rotta home 
 app.get("/", (req, res) => {
-    res.send("Benvenuto nella API del blog");
+    res.send("Server del mio Blog");
 })
 
+//rotta bacheca
+app.get("/bacheca", (req, res) => {
+    const prodotti = [
+        {
+            id: 1,
+            titolo: "ciambellone",
+            contenuto: "imagine ciambellone",
+            image: "images/ciambellone.jpeg",
+            tags: ["latte, uova, zucche"]
+        }, {
+            id: 2,
+            titolo: "cracker barbabietola",
+            contenuto: "imagine cracker di barbaabietola",
+            image: "public/images/cracker_barbabietola.jpeg",
+            tags: ["cracker, barbabietola"]
+        }, {
+            id: 3,
+            titolo: "pane fritto dolce",
+            contenuto: "imagine di pane fritto dolce",
+            image: "images/pane_fritto_dolce.jpeg",
+            tags: ["pane, dolce, zucchero"]
+        }, {
+            id: 4,
+            titolo: "pasta barbabietola",
+            contenuto: "imagine di pasta di barbabietole",
+            image: "images/pasta_barbabietola.jpeg",
+            tags: ["pasta, barbabietola"]
+        }, {
+            id: 5,
+            titolo: "torta paesana",
+            contenuto: "imagine torta paesana",
+            image: "images/torta_paesana.jpeg",
+            tags: ["torta, paese"]
+        }
+    ];
+
+    res.json(prodotti);
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
